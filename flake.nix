9c6689906e8dca4e7a9c45765094b89ad1842074
huantian <davidtianli@gmail.com>
2022-10-11 14:02:11 -0700

    wups

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     jtojnar.url = "github:huantianad/jtojnar-nixfiles";
-    # jtojnar.inputs.nixpkgs.follows = "nixpkgs";
+    jtojnar.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils, jtojnar, ... } @ inputs:
